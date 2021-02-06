ee3295e3d9c3d7af7492173f2596cf1efc9064c8
blee2125 <blee2125@gmail.com>
2021-02-06 16:54:52 -0500

    add route paths and components

--- storage-rental-app/src/containers/StorageUnitsContainer.js
+++ storage-rental-app/src/containers/StorageUnitsContainer.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+//import {fetchStorageUnits} from '../actions/fetchStorageUnits'
+import RentalForm from '../components/RentalForm'
+import RentalUnits from '../components/RentalUnits'
+import NewRentalUnit from '../components/NewRentalUnit'
 
 class StorageUnitsContainer extends React.Component{
+
+    componentDidMount(){
+        //this.props.fetchStorageUnits()
+    }
 
     render(){
         return(
             <div>
                 storage units container
+                <Switch>
+                <Route path='/rentalunits' component={RentalUnits} />
+                <Route path='/addrentalunit' component={NewRentalUnit} />
+                <Route path='/rentalform' component={RentalForm} />                    
+                </Switch>
             </div>
         )
     }
