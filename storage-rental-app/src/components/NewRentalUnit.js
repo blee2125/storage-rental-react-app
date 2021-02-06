e742b6cb22c437dc6c72c3aab61827cbf0e48634
blee2125 <blee2125@gmail.com>
2021-02-06 15:43:44 -0500

    add components to show and add units

---
+++ storage-rental-app/src/components/NewRentalUnit.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class NewRentalUnit extends React.Component{
+
+    //state= {unit_number: '', customer: '', comments: ''}
+
+    
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default (NewRentalUnit)
---
+++ storage-rental-app/src/components/RentalUnits.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class RentalUnits extends React.Component{
+
+    //state= {unit_number: '', customer: '', comments: ''}
+
+    
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default (RentalUnits)
