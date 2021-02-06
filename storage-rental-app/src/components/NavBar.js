87102202999528f44c0ddc6f8091dbacd65a4662
blee2125 <blee2125@gmail.com>
2021-02-06 11:23:50 -0500

    update navbar

--- storage-rental-app/src/components/NavBar.js
+++ storage-rental-app/src/components/NavBar.js
@@ -4,8 +4,10 @@ import {NavLink} from 'react-router-dom'
 const NavBar = () => {
     
     return(
-        <div>
-
+        <div className='navbar'>
+            <NavLink to='/rentalunits'>units</NavLink><br/>
+            <NavLink to='/rentalunits/new'>new/modify units</NavLink><br/>
+            <NavLink to='/rentalform'>rental form</NavLink><br/>
         </div>
     )
 }
