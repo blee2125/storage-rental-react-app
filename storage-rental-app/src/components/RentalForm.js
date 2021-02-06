af7bab9c52d2ef0afb6aa0cf4b5e740a32ae4c46
blee2125 <blee2125@gmail.com>
2021-02-06 14:34:49 -0500

    build rental form

--- storage-rental-app/src/components/RentalForm.js
+++ storage-rental-app/src/components/RentalForm.js
@@ -4,4 +4,31 @@ class RentalForm extends React.Component{
 
     state= {unit_number: '', customer: '', comments: ''}
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.rentUnit(this.state)
+        this.setState({unit_number: '', customer: '', comments: ''})
+    }
+
+    render(){
+        return(
+            <div>
+                <form className='RentalForm' onSubmit={this.handleSubmit}>
+                    Rental Form
+                    <label> Unit Number: </label>
+                    <input type='text' value={this.state.name} name='unit_number' onChange={this.handleChange} />
+                    <label> Customer: </label>
+                    <input type='text' value={this.state.name} name='customer' onChange={this.handleChange} />
+                    <label> Comments: </label>
+                    <input type='text' value={this.state.name} name='comments' onChange={this.handleChange} />
+                </form>
+            </div>
+        )
+    }
 }
