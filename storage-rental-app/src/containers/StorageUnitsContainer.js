ca4a2861920e5a2cb4e7c94a0dacf73fa5b98ee9
blee2125 <blee2125@gmail.com>
2021-02-06 11:51:53 -0500

    create StorageUnitsContainer

---
+++ storage-rental-app/src/containers/StorageUnitsContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class StorageUnitsContainer extends React.Component{
+
+    render(){
+        return(
+            <div>
+                storage units container
+            </div>
+        )
+    }
+}
+
+export default StorageUnitsContainer
