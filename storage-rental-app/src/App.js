77e4997a66cf3273b02c44fcf7ccff2bef4fbc52
blee2125 <blee2125@gmail.com>
2021-02-06 11:52:59 -0500

    add StorageUnitsContainer

--- storage-rental-app/src/App.js
+++ storage-rental-app/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from '../src/components/NavBar'
+import StorageUnitsContainer from '../src/containers/StorageUnitsContainer'
 
 function App() {
   return (
     <div className="App">
       <h1>Storage Units</h1>
       <NavBar />
+      <StorageUnitsContainer />
     </div>
   );
 }
