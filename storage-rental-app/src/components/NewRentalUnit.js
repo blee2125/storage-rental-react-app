f38133e0f9f888655679ac28731f5ede42d011a6
blee2125 <blee2125@gmail.com>
2021-02-06 16:49:29 -0500

    start new unit form

--- storage-rental-app/src/components/NewRentalUnit.js
+++ storage-rental-app/src/components/NewRentalUnit.js
@@ -2,14 +2,32 @@ import React from 'react'
 
 class NewRentalUnit extends React.Component{
 
-    //state= {unit_number: '', customer: '', comments: ''}
+    state= {
+        unit_number: '',
+        
+    }
 
-    
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addStorageUnit(this.state)
+        this.setState({unit_number: ''})
+    }
 
     render(){
         return(
             <div>
-                
+                <form className='NewStorageUnit' onSubmit={this.handleSubmit}>
+                    Add New Storage Unit<br/>
+                    <label> Unit Number: </label>
+                    <input type='text' value={this.state.name} name='unit_number' onChange={this.handleChange} />
+                    <input type='submit' />
+                </form>
             </div>
         )
     }
