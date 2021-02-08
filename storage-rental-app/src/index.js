ece62d0864ae663ac04a215998483828fef9ee44
blee2125 <blee2125@gmail.com>
2021-02-08 12:30:49 -0500

    add redux

--- storage-rental-app/src/index.js
+++ storage-rental-app/src/index.js
@@ -2,20 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import {BrowserRouter as Router} from 'react-router-dom'
 
+import {createStore} from 'redux'
+import {applyMiddleware} from 'redux'
+import {compose} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import unitReducer from '../src/reducers/unitReducer'
+
+const composeEnhancers= window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+let store = createStore(unitReducer, composeEnhancers(
+  applyMiddleware(thunk)
+))
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-    <App />
-    </Router>
+    <Provider store = {store}>
+      <Router>
+      <App />
+      </Router>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
