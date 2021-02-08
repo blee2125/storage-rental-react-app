298cf8333ccf9f68a45571300f72b9441ebd9642
blee2125 <blee2125@gmail.com>
2021-02-08 11:47:55 -0500

    create function to add units

--- storage-rental-app/src/actions/addStorageUnit.js
+++ storage-rental-app/src/actions/addStorageUnit.js
@@ -1,6 +1,11 @@
 export const addStorageUnit = () => {
 
     return(dispatch) => {
-        
+        fetch('http://localhost:3000/units',
+        {method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(unit)})
+        .then(resp => resp.json())
+        .then(unit => dispatch({type: 'ADD_UNIT', payload: unit}))
     }
 }
