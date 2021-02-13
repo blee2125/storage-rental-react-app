95c40e31bf4bad6c50eff7de5d9e0b0c4945bd3d
blee2125 <blee2125@gmail.com>
2021-02-13 16:45:09 -0500

    works with backend

--- storage-rental-app/src/actions/addStorageUnit.js
+++ storage-rental-app/src/actions/addStorageUnit.js
@@ -1,10 +1,11 @@
-export const addStorageUnit = () => {
+export const addStorageUnit = (data) => {
 
     return(dispatch) => {
         fetch('http://localhost:3000/units',
         {method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(unit)})
+        body: JSON.stringify(data)
+        })
         .then(resp => resp.json())
         .then(unit => dispatch({type: 'ADD_UNIT', payload: unit}))
     }
