addc4c515d11b2dc5833140f86678a4da7366852
blee2125 <blee2125@gmail.com>
2021-02-13 16:45:47 -0500

    works with backend

--- storage-rental-app/src/actions/rentStorageUnit.js
+++ storage-rental-app/src/actions/rentStorageUnit.js
@@ -1,6 +1,12 @@
-export const rentStorageUnit = () => {
+export const rentStorageUnit = (data) => {
 
     return(dispatch) => {
-        
+        fetch('http://localhost:3000/rental_forms',
+        {method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+        })
+        .then(resp => resp.json())
+        .then(unit => dispatch({type: 'RENT_UNIT', payload: unit}))
     }
 }
