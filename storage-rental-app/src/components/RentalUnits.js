8c13fac4fb1eecc5887d003f7d8660e019af3975
blee2125 <blee2125@gmail.com>
2021-02-16 09:09:58 -0500

    map works to sort units

--- storage-rental-app/src/components/RentalUnits.js
+++ storage-rental-app/src/components/RentalUnits.js
@@ -1,18 +1,35 @@
 import React from 'react'
 
+import StorageUnit from '../components/StorageUnit'
+//import {fetchStorageUnits} from '../actions/fetchStorageUnits'
+
 class RentalUnits extends React.Component{
-
-    //state= {unit_number: '', customer: '', comments: ''}
-
     
+    renderStorageUnits = (something) => something.map((unit, id) => <StorageUnit key={id} text={unit} />)
 
     render(){
-        return(
-            <div>
-                
+        if (!!this.props.units) {
+            return(
+                <div>
+                rental units
+                {console.log(this.props.units.storageUnits)}
+                {this.renderStorageUnits(this.props.units.storageUnits)}
+                {/* {this.renderStorageUnits(this.props.units.storageUnits)} */}
+
+                {/* <StorageUnit  /> */}
             </div>
-        )
+            )
+        } else {
+            return(
+                <div>
+                    no units
+                </div>
+            )
+        }
     }
 }
+
+    //renderStorageUnits = () => this.props.units.map((unit, id) => <StorageUnit key={id} text={unit} />)
+
 
 export default (RentalUnits)
