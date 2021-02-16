785dbfa742b55afa17d0c3658ee7d090cc58e556
blee2125 <blee2125@gmail.com>
2021-02-16 16:01:19 -0500

    deletes or throws error

--- storage-rental-app/src/actions/deleteStorageUnit.js
+++ storage-rental-app/src/actions/deleteStorageUnit.js
@@ -1,10 +1,14 @@
 export const deleteStorageUnit = (unitId) => {
 
     return(dispatch) => {
-        fetch('http://localhost:3000/units/${unitId}',
-        {method: 'DELETE'
-        })
+        fetch(`http://localhost:3000/units/${unitId}`,
+        {method: 'DELETE', headers: {'Content-Type': 'application/json'}})
         .then(resp => resp.json())
-        .then(unit => dispatch({type: 'DELETE_UNIT', payload: unit}))
+        .then(data => dispatch({type: 'DELETE_UNIT', payload: data}))
+        .catch(error => {
+            console.error({
+              error
+            });
+          });
     }
 }
