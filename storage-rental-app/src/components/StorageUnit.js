7ea32867dba9abca4b5dc1e826a8114e257faee2
blee2125 <blee2125@gmail.com>
2021-02-16 15:57:22 -0500

    add delete button

--- storage-rental-app/src/components/StorageUnit.js
+++ storage-rental-app/src/components/StorageUnit.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import {deleteStorageUnit} from '../actions/deleteStorageUnit'
 
 const StorageUnit = (props) => {
     
-    // console.log(props.text.unit_number)
+    const handleDelete = (storageUnit) => {
+        //console.log(storageUnit.text.unit_number)
+        props.deleteStorageUnit(storageUnit.text.id)
+    }
 
     return(
         <div>
-            <ul>Unit Number: {props.text.unit_number}</ul>
+            <ul>Unit Number: {props.text.unit_number}</ul><button onClick={()=> handleDelete(props)}>delete</button>
         </div>
     )
 }
 
-export default StorageUnit
+export default connect(null, {deleteStorageUnit})(StorageUnit)
