7d785f602407cc0b5f1d1effe74868aef6ffbb52
blee2125 <blee2125@gmail.com>
2021-02-16 15:55:03 -0500

    DELETE_UNIT now renders on page

--- storage-rental-app/src/reducers/unitReducer.js
+++ storage-rental-app/src/reducers/unitReducer.js
@@ -1,22 +1,19 @@
 export default function unitReducer(state = {units: []}, action){
-
+//console.log(action)
     switch (action.type){
         case 'FETCH_UNITS':
+            //console.log('fetch units')
             return {units: action.payload}
         case 'ADD_UNIT':
-            console.log(state)
+            //console.log('add unit')
             return {...state, units: [...state.units, action.payload]}
             
         case 'DELETE_UNIT':
-            let unit = state.units.map(unit => {
-                if(unit.id == action.payload.id){
-                    return action.payload
-                } else {
-                    return unit
-                }
-            })
-            return {...state, units: unit}
+            //console.log('delete unit')
+            return {...state, units: state.units.filter(unit => unit.id !== action.payload.id)}
+            
         default:
+            //console.log('default')
             return state
     }
 
