0819c7d5a5194b6a0fd1a14926dc096f028fbef8
blee2125 <blee2125@gmail.com>
2021-02-16 13:09:31 -0500

    ADD_UNIT doesn't require page refresh

--- storage-rental-app/src/reducers/unitReducer.js
+++ storage-rental-app/src/reducers/unitReducer.js
@@ -4,7 +4,18 @@ export default function unitReducer(state = {units: []}, action){
         case 'FETCH_UNITS':
             return {units: action.payload}
         case 'ADD_UNIT':
-            return {units: action.payload}
+            console.log(state)
+            return {...state, units: [...state.units, action.payload]}
+            
+        case 'DELETE_UNIT':
+            let unit = state.units.map(unit => {
+                if(unit.id == action.payload.id){
+                    return action.payload
+                } else {
+                    return unit
+                }
+            })
+            return {...state, units: unit}
         default:
             return state
     }
