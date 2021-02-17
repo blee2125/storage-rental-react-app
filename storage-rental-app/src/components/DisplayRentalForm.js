e2458655ce5b4ead1ebc70c174db981e37456ed5
blee2125 <blee2125@gmail.com>
2021-02-17 15:51:11 -0500

    first commit

---
+++ storage-rental-app/src/components/DisplayRentalForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+
+const DisplayRentalForm = (props) => {
+    
+    console.log(props)
+
+    return(
+        <div>
+            <p>display rental</p>
+            
+        </div>
+    )
+}
+
+export default DisplayRentalForm
