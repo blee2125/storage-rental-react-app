f9cb4d712a04865f122c252084676ce9846d5e89
blee2125 <blee2125@gmail.com>
2021-02-18 10:00:11 -0500

    clean up

--- storage-rental-app/src/components/RentalUnits.js
+++ storage-rental-app/src/components/RentalUnits.js
@@ -1,35 +1,19 @@
 import React from 'react'
-
 import StorageUnit from '../components/StorageUnit'
-//import {fetchStorageUnits} from '../actions/fetchStorageUnits'
 
 class RentalUnits extends React.Component{
     
     renderStorageUnits = (something) => something.map((unit, id) => <StorageUnit key={id} text={unit} />)
 
     render(){
-        if (!!this.props.units) {
-            return(
-                <div>
-                rental units
-                {console.log(this.props.units.storageUnits)}
-                {this.renderStorageUnits(this.props.units.storageUnits)}
-                {/* {this.renderStorageUnits(this.props.units.storageUnits)} */}
+        return(
+            <div>
+                rental unit index
+                {this.renderStorageUnits(this.props.data.storageUnits)}
 
-                {/* <StorageUnit  /> */}
             </div>
-            )
-        } else {
-            return(
-                <div>
-                    no units
-                </div>
-            )
-        }
+        )
     }
 }
-
-    //renderStorageUnits = () => this.props.units.map((unit, id) => <StorageUnit key={id} text={unit} />)
-
 
 export default (RentalUnits)
