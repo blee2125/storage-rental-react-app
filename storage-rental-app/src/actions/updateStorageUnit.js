96bcf1e2f0dd017491dc06697c54780fa5bf61ac
blee2125 <blee2125@gmail.com>
2021-02-18 09:44:56 -0500

    update units on backend

---
+++ storage-rental-app/src/actions/updateStorageUnit.js
@@ -0,0 +1,12 @@
+export const updateStorageUnit = (data) => {
+
+    return(dispatch) => {
+        fetch(`http://localhost:3000/units/${id}`,
+        {method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+        })
+        .then(resp => resp.json())
+        .then(unit => dispatch({type: 'RENT_UNIT', payload: unit}))
+    }
+}
