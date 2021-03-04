85cdc5a0621594cc24ce853593815a469a2a4d8b
blee2125 <blee2125@gmail.com>
2021-03-04 15:40:15 -0500

    add sort button

--- storage-rental-app/src/components/RentalUnits.js
+++ storage-rental-app/src/components/RentalUnits.js
@@ -2,13 +2,26 @@ import React from 'react'
 import StorageUnit from '../components/StorageUnit'
 
 class RentalUnits extends React.Component{
+
+    state={
+        unitArray: this.props.data.storageUnits
+    }
     
     renderStorageUnits = (something) => something.map((unit, id) => <StorageUnit key={id} text={unit} />)
+
+    sortAbc = () => {
+        const {unitArray} = this.state
+        let sortUnits = unitArray.sort(function(a,b){
+            return a.unit_number.localeCompare(b.unit_number);
+        })
+        this.setState({unitArray: sortUnits})
+    }
 
     render(){
         return(
             <div>
-                rental unit index
+                Index
+                <button onClick={() => this.sortAbc(this.state)}>Sort</button>
                 {this.renderStorageUnits(this.props.data.storageUnits)}
 
             </div>
