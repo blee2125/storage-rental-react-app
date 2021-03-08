2f9049b1970cc715fd7a1119b7fc47fc069f9c89
blee2125 <blee2125@gmail.com>
2021-03-08 13:29:45 -0500

    change 'something'

--- storage-rental-app/src/containers/RentalFormsContainer.js
+++ storage-rental-app/src/containers/RentalFormsContainer.js
@@ -1,24 +1,19 @@
 import React from 'react'
 import DisplayRentalForm from '../components/DisplayRentalForm'
-
-import {fetchRentalForms} from '../actions/fetchRentalForms'
-import {connect} from 'react-redux'
 import RentalForm from '../components/RentalForm'
-
 
 class RentalFormsContainer extends React.Component{
 
 
-    renderDisplayRentalForm = (something) => something.map((form, id) => <DisplayRentalForm key={id} text={form} />)
+    renderDisplayRentalForm = (forms) => forms.map((form, id) => <DisplayRentalForm key={id} text={form} />)
 
 
     render(){
         return(
             <div>
-                rental form container
                 
-                <RentalForm data={this.props} />
-
+                <RentalForm data={this.props} /><br></br>
+Past rentals
                 <table>
                 <thead>
                 <tr>
