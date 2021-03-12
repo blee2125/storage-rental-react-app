0e81960ab668734e34b5a30fca236bdbea44f794
blee2125 <blee2125@gmail.com>
2021-03-11 19:46:39 -0500

    add title and change style

--- storage-rental-app/src/components/NavBar.js
+++ storage-rental-app/src/components/NavBar.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar'
 
 const NavBar = () => {
     
     return(
-        <div className='navbar'>
-            <NavLink to='/storageunits'>units</NavLink><br/>
-            <NavLink to='/addrentalunit'>add new units</NavLink><br/>
-            <NavLink to='/rentalform'>rental form</NavLink><br/>
-        </div>
+        <Navbar bg="dark" variant="dark">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">Storage Unit Manager</a>
+                    </div>
+                    <ul class="nav navbar-nav">
+                        <li><NavLink to='/storageunits'>units</NavLink></li>
+                        <li><NavLink to='/addrentalunit'>add new units</NavLink></li>
+                        <li><NavLink to='/rentalform'>rental form</NavLink></li>
+                    </ul>
+                </div>
+            </nav>
+        </Navbar>
     )
 }
 
